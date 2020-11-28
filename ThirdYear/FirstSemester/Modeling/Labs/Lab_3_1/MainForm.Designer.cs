96ab9ff4ee5a3e832057d5a303775e6303a49166
Artur Boyun <arturboyun@gmail.com>
2020-11-28 14:58:53 +0200

    feat: Lab_3_1

---
+++ ThirdYear/FirstSemester/Modeling/Labs/Lab_3_1/MainForm.Designer.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.Win32;
+
+namespace Lab_3_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.chooseCuisineBox = new System.Windows.Forms.ComboBox();
+            this.resultTextBox = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.chooseEatBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label1.Location = new System.Drawing.Point(15, 15);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Выберите кухню:";
+            // 
+            // chooseCuisineBox
+            // 
+            this.chooseCuisineBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.chooseCuisineBox.FormattingEnabled = true;
+            this.chooseCuisineBox.Location = new System.Drawing.Point(18, 33);
+            this.chooseCuisineBox.Margin = new System.Windows.Forms.Padding(3, 3, 3, 15);
+            this.chooseCuisineBox.Name = "chooseCuisineBox";
+            this.chooseCuisineBox.Size = new System.Drawing.Size(265, 21);
+            this.chooseCuisineBox.TabIndex = 1;
+            this.chooseCuisineBox.SelectedIndexChanged += new System.EventHandler(this.chooseCuisineBox_SelectedIndexChanged);
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Location = new System.Drawing.Point(18, 142);
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.Size = new System.Drawing.Size(265, 268);
+            this.resultTextBox.TabIndex = 2;
+            this.resultTextBox.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(18, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Результат:";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label3.Location = new System.Drawing.Point(15, 69);
+            this.label3.Margin = new System.Windows.Forms.Padding(0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 15);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Выберите блюдо:";
+            // 
+            // chooseEatBox
+            // 
+            this.chooseEatBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.chooseEatBox.Enabled = false;
+            this.chooseEatBox.FormattingEnabled = true;
+            this.chooseEatBox.Location = new System.Drawing.Point(18, 87);
+            this.chooseEatBox.Margin = new System.Windows.Forms.Padding(3, 3, 3, 15);
+            this.chooseEatBox.Name = "chooseEatBox";
+            this.chooseEatBox.Size = new System.Drawing.Size(265, 21);
+            this.chooseEatBox.TabIndex = 1; 
+            this.chooseEatBox.SelectedIndexChanged += new System.EventHandler(this.chooseEatBox_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(301, 428);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.chooseEatBox);
+            this.Controls.Add(this.chooseCuisineBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(15);
+            this.Text = "Меню";
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.ComboBox chooseEatBox;
+
+        private System.Windows.Forms.Label label3;
+
+        private System.Windows.Forms.ComboBox chooseCuisineBox;
+        private System.Windows.Forms.RichTextBox resultTextBox;
+
+        private System.Windows.Forms.Label label2;
+
+        private System.Windows.Forms.Label label1;
+
+        #endregion
+    }
+}
---
+++ ThirdYear/FirstSemester/Modeling/Labs/Lab_3_1/MainForm.cs
@@ -0,0 +1,392 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+/*
+TODO:
+Застосувати патерн Абстрактна фабрика 
+для сімейств класів Основні страви та Десерти, 
+створивши фабрики їжі для української кухні та східної кухні.
+
+Factories:
+    abstract classes:
+        - СuisineFactory
+        
+    classes:
+        - UkrainianСuisineFactory
+        - EasternСuisineFactory
+    
+Products:
+    abstract classes:
+        - Soup
+        - Dumplings
+        - Cake
+        - IceCream
+        
+    classes:
+        - UkrainianSoup
+        - UkrainianDumplings
+        - UkrainianCake
+        - UkrainianIceCream
+    
+        - EasternSoup
+        - EasternDumplings
+        - EasternCake
+        - EasternIceCream
+*/
+
+
+namespace Lab_3_1
+{
+    abstract class СuisineFactory
+    {
+        public abstract Soup CreateSoup();
+        public abstract Dumplings CreateDumplings();
+        public abstract Cake CreateCake();
+        public abstract IceCream CreateIceCream();
+    }
+
+    class UkrainianСuisine : СuisineFactory
+    {
+        public override Soup CreateSoup()
+        {
+            return new UkrainianSoup();
+        }
+
+        public override Dumplings CreateDumplings()
+        {
+            return new UkrainianDumplings();
+        }
+
+        public override Cake CreateCake()
+        {
+            return new UkrainianCake();
+        }
+
+        public override IceCream CreateIceCream()
+        {
+            return new UkrainianIceCream();
+        }
+    }
+
+    class EasternСuisine : СuisineFactory
+    {
+        public override Soup CreateSoup()
+        {
+            return new EasternSoup();
+        }
+
+        public override Dumplings CreateDumplings()
+        {
+            return new EasternDumplings();
+        }
+
+        public override Cake CreateCake()
+        {
+            return new EasternCake();
+        }
+
+        public override IceCream CreateIceCream()
+        {
+            return new EasternIceCream();
+        }
+    }
+    
+    abstract class Soup
+    {
+        public string category = "Первое блюдо";
+        public abstract string Name { get; }
+        public abstract string GetIngredients();
+    }
+
+    abstract class Dumplings
+    {
+        public string category = "Второе блюдо";
+        public abstract string Name { get; }
+        public abstract string GetIngredients();
+    }
+
+    abstract class Cake
+    {
+        public string category = "Десерт";
+        public abstract string Name { get; }
+        public abstract string GetIngredients();
+    }
+
+    abstract class IceCream
+    {
+        public string category = "Десерт";
+        public abstract string Name { get; }
+        public abstract string GetIngredients();
+    }
+
+    class UkrainianSoup : Soup
+    {
+        public override string Name { get; }
+
+        public UkrainianSoup()
+        {
+            Name = "Украинский суп";
+        }
+
+        public override string GetIngredients()
+        {
+            return "Картофель - 3 шт.\n" +
+                   "Вода - 1,5-2 л\n" +
+                   "Морковь - 1 шт.\n" +
+                   "Лук репчатый - 1 шт.\n" +
+                   "Масло сливочное - 50 г\n" +
+                   "Лавровый лист - 1 шт.\n" +
+                   "Перец черный - 1 щепотка\n" +
+                   "Зелень укропа - 3 веточки\n" +
+                   "Зелень петрушки - 3 веточки\n" +
+                   "Соль - 0,5 ч. ложки\n" +
+                   "Мука - 100-120 г\n" +
+                   "Яйца - 1 шт.";
+        }
+    }
+
+    class UkrainianDumplings : Dumplings
+    {
+        public override string Name { get; }
+
+        public UkrainianDumplings()
+        {
+            Name = "Украинские пельмени";
+        }
+
+        public override string GetIngredients()
+        {
+            return "Для начинки:\n" +
+                   "Фарш мясной (курица + свинина) – 700 г\n" +
+                   "Лук репчатый – 1 шт.\n" +
+                   "Чеснок – 2 зубчика\n" +
+                   "Зелень укропа – 1 небольшой пучок\n" +
+                   "Перец чёрный молотый – по вкусу\n" +
+                   "Соль – по вкусу\n\n" +
+                   "Для теста (стакан - 250 мл):\n" +
+                   "Мука – 3 стакана\n" +
+                   "Яйцо – 1 шт.\n" +
+                   "Масло растительное – 3 ст. ложки\n" +
+                   "Соли – 0,5 ч. ложки\n" +
+                   "Кипяток – 1 стакан";
+        }
+    }
+
+    class UkrainianCake : Cake
+    {
+        public override string Name { get; }
+
+        public UkrainianCake()
+        {
+            Name = "Киевский торт";
+        }
+
+        public override string GetIngredients()
+        {
+            return "Для коржей:\n" +
+                   "Белки яичные – 12 шт.\n" +
+                   "Мука – 100 г\n" +
+                   "Сахар – 500 г\n" +
+                   "Орехи – 350 г\n" +
+                   "Ванильный сахар – 20 г\n\n" +
+                   "Для крема:\n" +
+                   "Масло сливочное – 500 г\n" +
+                   "Желтки – 12 шт.\n" +
+                   "Молоко – 350 г\n" +
+                   "Сахар – 300 г\n" +
+                   "Какао – 20 г\n" +
+                   "Ванильный сахар – 10 г\n" +
+                   "Мука – 4 ст. л.\n" +
+                   "Коньяк – 2 ст. ложки\n" +
+                   "Для украшения:\n" +
+                   "Шоколад - 1 плитка";
+        }
+    }
+
+    class UkrainianIceCream : IceCream
+    {
+        public override string Name { get; }
+
+        public UkrainianIceCream()
+        {
+            Name = "Украинский пломбир";
+        }
+
+        public override string GetIngredients()
+        {
+            return "Молоко - 300 мл\n" +
+                   "Сливки - 300 мл\n" +
+                   "Яйца куриные - 4 шт.\n" +
+                   "Сахар - 120 г\n" +
+                   "Сахар ванильный - 1/2 ч. ложки\n" +
+                   "Шоколад для украшения - 50 г\n";
+        }
+    }
+
+    class EasternSoup : Soup
+    {
+        public override string Name { get; }
+
+        public EasternSoup()
+        {
+            Name = "Суп-лагман";
+        }
+
+        public override string GetIngredients()
+        {
+            return "Мякоть говяжья - 400 г\n" +
+                   "Лук - 1 шт.\n" +
+                   "Редька - 1 шт.\n" +
+                   "Помидоры - 2 шт.\n" +
+                   "или томат-паста - 1-2 ст. ложки\n" +
+                   "Вода - 2,5 стакана\n" +
+                   "Чеснок - 6 зубчиков\n" +
+                   "Масло растительное - 30 г\n" +
+                   "Соль - по вкусу\n" +
+                   "Перец красный молотый - по вкусу\n" +
+                   "Зелень - по вкусу\n\n" +
+                   "Для лапши:\n" +
+                   "Мука - 2 стакана\n" +
+                   "Вода - 0,7-1 стакан\n" +
+                   "Соль - 1 щепотка\n" +
+                   "Масло растительное - 1-2 ч. ложки\n" +
+                   "или готовая лапша - 400 г\n";
+        }
+    }
+
+    class EasternDumplings : Dumplings
+    {
+        public override string Name { get; }
+
+        public EasternDumplings()
+        {
+            Name = "Манты";
+        }
+
+        public override string GetIngredients()
+        {
+            return "Телятина - 1 кг\n" +
+                   "Лук репчатый - 3 шт.\n" +
+                   "Чеснок - 3 зубчика\n" +
+                   "Мука - 0,5 кг\n" +
+                   "Яйцо – 1 шт.\n" +
+                   "Вода - сколько возьмет тесто\n" +
+                   "Соль - по вкусу\n" +
+                   "Смесь свежемолотых перцев – 1 ч.л. (по вкусу)\n" +
+                   "Черный свежемолотый перец – 1 ч.л. (по вкусу)\n";
+        }
+    }
+
+    class EasternCake : Cake
+    {
+        public override string Name { get; }
+
+        public EasternCake()
+        {
+            Name = "Турецкий маковый торт";
+        }
+
+        public override string GetIngredients()
+        {
+            return "Для сиропа\n" +
+                   "200 г сахар\n" +
+                   "250 мл вода\n" +
+                   "несколько капель лимонный сок\n\n" +
+                   "Для теста\n" +
+                   "4 шт. яйца\n" +
+                   "200 г сахар\n" +
+                   "200 г манная крупа\n" +
+                   "170 г мак\n" +
+                   "250 г масло растительное\n\n" +
+                   "Для крема\n" +
+                   "250 г творог обезжиренный\n" +
+                   "6 ч. л. San Apart стабилизатор* (не обязательно)\n" +
+                   "400 г сливки охлажденные\n\n" +
+                   "Для украшения\n" +
+                   "кокосовая стружка\n";
+        }
+    }
+
+    class EasternIceCream : IceCream
+    {
+        public override string Name { get; }
+
+        public EasternIceCream()
+        {
+            Name = "Дондурма";
+        }
+
+        public override string GetIngredients()
+        {
+            return "Молоко (лучше всего козье, но и коровье подойдет) – 500 мл.\n" +
+                   "Сахар —  170 гр.\n" +
+                   "Салеп – 1,5 чайной ложки.\n" +
+                   "Ванильный сахар  — 1 пакетик .";
+        }
+    }
+
+    public partial class Form1 : Form
+    {
+        СuisineFactory _cuisine;
+        public Form1()
+        {
+            InitializeComponent();
+            chooseCuisineBox.Items.Add("Украинская Кухня");
+            chooseCuisineBox.Items.Add("Восточная Кухня");
+            chooseEatBox.Items.Clear();
+            chooseEatBox.Enabled = false;
+        }
+
+        private void chooseCuisineBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            _cuisine = chooseCuisineBox.SelectedIndex switch
+            {
+                0 => new UkrainianСuisine(),
+                1 => new EasternСuisine(),
+                _ => _cuisine
+            };
+            FillChooseEatBox(_cuisine);
+        }
+
+        private void chooseEatBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string text = "";
+            switch (chooseEatBox.SelectedIndex)
+            {
+                case 0:
+                    var soup = _cuisine.CreateSoup();
+                    text = $"Название: {soup.Name}\n\n";
+                    text += $"Ингридиенты:\n{soup.GetIngredients()}";
+                    break;
+                case 1:
+                    var dumplings = _cuisine.CreateDumplings();
+                    text = $"Название: {dumplings.Name}\n\n";
+                    text += $"Ингридиенты:\n{dumplings.GetIngredients()}";
+                    break;
+                case 2:
+                    var cake = _cuisine.CreateCake();
+                    text = $"Название: {cake.Name}\n\n";
+                    text += $"Ингридиенты:\n{cake.GetIngredients()}";
+                    break;
+                case 3:
+                    var icecream = _cuisine.CreateIceCream();
+                    text = $"Название: {icecream.Name}\n\n";
+                    text += $"Ингридиенты:\n{icecream.GetIngredients()}";
+                    break;
+            }
+            resultTextBox.Text = text;
+        }
+
+        private void FillChooseEatBox(СuisineFactory cuisine)
+        {
+            chooseEatBox.Items.Clear();
+            chooseEatBox.Items.Add(cuisine.CreateSoup().Name);
+            chooseEatBox.Items.Add(cuisine.CreateDumplings().Name);
+            chooseEatBox.Items.Add(cuisine.CreateCake().Name);
+            chooseEatBox.Items.Add(cuisine.CreateIceCream().Name);
+            chooseEatBox.Enabled = true;
+            chooseEatBox.SelectedIndex = 0;
+        }
+    }
+}
